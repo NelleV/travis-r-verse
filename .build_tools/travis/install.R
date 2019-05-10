542fd4733df688e44c1171cbd21fe6787676c8cb
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-05-10 11:11:11 -0700

    FIX travis

---
+++ .build_tools/travis/install.R
@@ -0,0 +1,15 @@
+if(!require("devtools")){
+    install.packages("devtools")
+}
+
+if(!require(ggfortify)){
+    install.packages("ggfortify")
+}
+
+if(!require(kableExtra)){
+    install.packages("kableExtra")
+}
+
+if(!require(testthat)){
+    install.packages("testthat")
+}
